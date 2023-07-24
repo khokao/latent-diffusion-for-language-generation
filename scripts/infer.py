397986af11492d86c0d5e88e969c853c930bf742
khokao <kokihokao@gmail.com>
2023-07-24 14:26:12 +0900

    [scripts] Directory related fixes

--- scripts/infer.py
+++ scripts/infer.py
@@ -1,28 +1,38 @@
+import shutil
 from pathlib import Path
 
 import hydra
+from accelerate.logging import get_logger
+from hydra.core.hydra_config import HydraConfig
 from omegaconf import OmegaConf
 
 from ld4lg.cfg import MyConfig
 
+logger = get_logger(__name__)
+
 
 @hydra.main(version_base=None, config_path='../ld4lg/cfg', config_name='config')
-def main(hydra_cfg: MyConfig):
-    assert hydra_cfg.infer.output_dir is not None, 'Please specify `infer.output_dir`.'
+def main(cfg: MyConfig):
+    assert cfg.infer.output_dir is not None, 'Please specify `infer.output_dir`.'
 
-    output_dir = Path(hydra_cfg.infer.output_dir)
-    cfg_path = output_dir / hydra_cfg.infer.cfg_path
-    ckpt_path = output_dir / hydra_cfg.infer.ckpt_path
+    output_dir = Path(cfg.infer.output_dir)
+    cfg_path = output_dir / cfg.infer.cfg_path
+    ckpt_path = output_dir / cfg.infer.ckpt_path
 
     loaded_cfg = OmegaConf.load(cfg_path)  # Config saved during training.
-    loaded_cfg.generation = hydra_cfg.generation  # Overwrite `generation` key in saved config with hydra config.
-    loaded_cfg.infere = hydra_cfg.infer  # Overwrite `infer` key in saved config with hydra config.
+    loaded_cfg.generation = cfg.generation  # Overwrite `generation` key in saved config with hydra config.
+    loaded_cfg.infer = cfg.infer  # Overwrite `infer` key in saved config with hydra config.
 
     module = __import__('ld4lg', fromlist=['LD4LGInterface'])
     interface_cls = getattr(module, 'LD4LGInterface')
     interface = interface_cls(loaded_cfg, output_dir, mode='infer', ckpt_path=ckpt_path)
 
     interface.infer()
+
+    hydra_cfg = HydraConfig.get()
+    created_dir = Path(hydra_cfg.run.dir)
+    shutil.copy(created_dir / f'{hydra_cfg.job.name}.log', output_dir)
+    shutil.rmtree(created_dir)
 
 
 if __name__ == '__main__':
--- scripts/test.py
+++ scripts/test.py
@@ -1,32 +1,38 @@
-import argparse
+import shutil
 from pathlib import Path
 
+import hydra
+from accelerate.logging import get_logger
+from hydra.core.hydra_config import HydraConfig
 from omegaconf import OmegaConf
 
+from ld4lg.cfg import MyConfig
 
-def get_args():
-    parser = argparse.ArgumentParser()
-    parser.add_argument('-o', '--output', type=str, required=True)
-    parser.add_argument('-c', '--ckpt', type=str, default='checkpoints/pytorch_model_1.bin')
-    args = parser.parse_args()
-    return args
+logger = get_logger(__name__)
 
 
-def main():
-    args = vars(get_args())
-    print(args)
+@hydra.main(version_base=None, config_path='../ld4lg/cfg', config_name='config')
+def main(cfg: MyConfig):
+    assert cfg.infer.output_dir is not None, 'Please specify `infer.output_dir`.'
 
-    output_dir = Path(args['output'])
-    cfg_path = output_dir / '.hydra' / 'config.yaml'
-    ckpt_path = output_dir / args['ckpt']
+    output_dir = Path(cfg.infer.output_dir)
+    cfg_path = output_dir / cfg.infer.cfg_path
+    ckpt_path = output_dir / cfg.infer.ckpt_path
 
-    cfg = OmegaConf.load(cfg_path)
+    loaded_cfg = OmegaConf.load(cfg_path)  # Config saved during training.
+    loaded_cfg.generation = cfg.generation  # Overwrite `generation` key in saved config with hydra config.
+    loaded_cfg.infer = cfg.infer  # Overwrite `infer` key in saved config with hydra config.
 
     module = __import__('ld4lg', fromlist=['LD4LGInterface'])
     interface_cls = getattr(module, 'LD4LGInterface')
-    interface = interface_cls(cfg, output_dir, mode='test', ckpt_path=ckpt_path)
+    interface = interface_cls(loaded_cfg, output_dir, mode='test', ckpt_path=ckpt_path)
 
     interface.test()
+
+    hydra_cfg = HydraConfig.get()
+    created_dir = Path(hydra_cfg.run.dir)
+    shutil.copy(created_dir / f'{hydra_cfg.job.name}.log', output_dir)
+    shutil.rmtree(created_dir)
 
 
 if __name__ == '__main__':
